027f2c8d97dac797ed478ca904a15d4557c79e5e
Davi S. Bento <contato@davisbento.dev>
2022-07-29 16:48:45 -0300

    feat: added links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,38 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 
-const Home: NextPage = ({ blogs }: any) => {
-	console.log(blogs);
+const Home: NextPage = () => {
 	return (
 		<div>
 			<Head>
 				<title>Davi S. Bento</title>
 				<meta name='description' content="Davi's Blog" />
-				<link rel='icon' href='/favicon.ico' />
+				<link rel='icon' href='/avatar.png' />
 			</Head>
 
-			<main className='flex flex-col items-center justify-center'>
-				<h1 className='text-2xl'>Posts</h1>
+			<main className='flex flex-col items-center justify-center mt-16 gap-8'>
+				<div className='flex flex-col items-center'>
+					<h1 className='text-2xl text-white'>Davi S. Bento - Senior Full Stack Developer</h1>
+					<Image src='/avatar.png' alt='Avatar Image' height={150} width={150} />
+				</div>
 
-				<ul>
+				<div>
+					<ul className='flex flex-col items-center'>
+						<li className='text-lg'>
+							<a href='https://github.com/davisbento' target='_blank' rel='noopener noreferrer'>
+								Github
+							</a>
+						</li>
+						<li className='text-lg'>
+							<a href='https://linkedin.com/in/davisbento' target='_blank' rel='noopener noreferrer'>
+								Linkedin
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				{/* <ul>
 					{blogs.map((blog: any) => {
 						return (
 							<li key={blog.id}>
@@ -26,37 +42,37 @@ const Home: NextPage = ({ blogs }: any) => {
 							</li>
 						);
 					})}
-				</ul>
+				</ul> */}
 			</main>
 		</div>
 	);
 };
 
 // This function gets called at build time on server-side.
-export async function getStaticProps() {
-	const fs = require('fs');
-	const matter = require('gray-matter');
-	const { v4: uuid } = require('uuid');
+// export async function getStaticProps() {
+// 	const fs = require('fs');
+// 	const matter = require('gray-matter');
+// 	const { v4: uuid } = require('uuid');
 
-	const files = fs.readdirSync(`${process.cwd()}/contents`, 'utf-8');
+// 	const files = fs.readdirSync(`${process.cwd()}/contents`, 'utf-8');
 
-	const blogs = files
-		.filter((fn: any) => fn.endsWith('.md'))
-		.map((fn: any) => {
-			const path = `${process.cwd()}/contents/${fn}`;
-			const rawContent = fs.readFileSync(path, {
-				encoding: 'utf-8'
-			});
-			const { data } = matter(rawContent);
+// 	const blogs = files
+// 		.filter((fn: any) => fn.endsWith('.md'))
+// 		.map((fn: any) => {
+// 			const path = `${process.cwd()}/contents/${fn}`;
+// 			const rawContent = fs.readFileSync(path, {
+// 				encoding: 'utf-8'
+// 			});
+// 			const { data } = matter(rawContent);
 
-			return { ...data, id: uuid() };
-		});
+// 			return { ...data, id: uuid() };
+// 		});
 
-	// By returning { props: blogs }, the IndexPage component
-	// will receive `blogs` as a prop at build time
-	return {
-		props: { blogs }
-	};
-}
+// 	// By returning { props: blogs }, the IndexPage component
+// 	// will receive `blogs` as a prop at build time
+// 	return {
+// 		props: { blogs }
+// 	};
+// }
 
 export default Home;
